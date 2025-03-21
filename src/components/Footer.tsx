88ff1690c4676e771d96272afeb3a62e4cd7c882
Ellie Ishii <166664143+ellieishii@users.noreply.github.com>
2025-03-20 22:20:12 -1000

    Update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,31 +3,29 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import { House, Grid, Receipt, Person } from 'react-bootstrap-icons';
 
-const Footer = () => {
-  return (
-    <footer className="footer">
-      <Container fluid>
-        <Row className="text-center">
-          <Col>
-            <House size={24} />
-            <div className="footer-label">Home</div>
-          </Col>
-          <Col>
-            <Grid size={24} />
-            <div className="footer-label">Services</div>
-          </Col>
-          <Col>
-            <Receipt size={24} />
-            <div className="footer-label">Activity</div>
-          </Col>
-          <Col>
-            <Person size={24} />
-            <div className="footer-label">Account</div>
-          </Col>
-        </Row>
-      </Container>
-    </footer>
-  );
-};
+const Footer = () => (
+  <footer className="footer">
+    <Container fluid>
+      <Row className="text-center">
+        <Col>
+          <House size={24} />
+          <div className="footer-label">Home</div>
+        </Col>
+        <Col>
+          <Grid size={24} />
+          <div className="footer-label">Services</div>
+        </Col>
+        <Col>
+          <Receipt size={24} />
+          <div className="footer-label">Activity</div>
+        </Col>
+        <Col>
+          <Person size={24} />
+          <div className="footer-label">Account</div>
+        </Col>
+      </Row>
+    </Container>
+  </footer>
+);
 
 export default Footer;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,21 @@
-"use client";
+'use client';
 
-import Image from "next/image";
-import { Container, Navbar } from "react-bootstrap";
+import Image from 'next/image';
+import { Container, Navbar } from 'react-bootstrap';
 
-const NavBar: React.FC = () => {
-  return (
-    <Navbar expand="lg" className="custom-navbar justify-content-between px-1">
-      <Container fluid>
-        {/* Logo on the left with "Ride On" text */}
-        <Navbar.Brand href="/" className="d-flex align-items-center">
-          <Image src="/rideon.png" alt="Logo" width={45} height={35} priority />
-          <span className="brand-text">Ride On</span>
-        </Navbar.Brand>
+const NavBar: React.FC = () => (
+  <Navbar expand="lg" className="custom-navbar justify-content-between px-1">
+    <Container fluid>
+      {/* Logo on the left with "Ride On" text */}
+      <Navbar.Brand href="/" className="d-flex align-items-center">
+        <Image src="/rideon.png" alt="Logo" width={45} height={35} priority />
+        <span className="brand-text">Ride On</span>
+      </Navbar.Brand>
 
-        {/* Static User Name "Sara" on the right */}
-        <span className="user-name">Sara</span>
-      </Container>
-    </Navbar>
-  );
-};
+      {/* Static User Name "Sara" on the right */}
+      <span className="user-name">Sara</span>
+    </Container>
+  </Navbar>
+);
 
 export default NavBar;
