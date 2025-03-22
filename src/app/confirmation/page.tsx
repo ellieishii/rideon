bd9361cf6b86fe352faaec12c9858be2d61cbc3c
Ellie Ishii <166664143+ellieishii@users.noreply.github.com>
2025-03-21 22:15:26 -1000

    Updated confirmation page

--- src/app/confirmation/page.tsx
+++ src/app/confirmation/page.tsx
@@ -2,11 +2,7 @@
 
 import React from 'react';
 import './style.css';
-import {
-  CardHeading,
-  CarFrontFill,
-  House,
-} from 'react-bootstrap-icons';
+import { CardHeading, CarFrontFill, House } from 'react-bootstrap-icons';
 
 export default function ConfirmationPage() {
   return (
@@ -14,7 +10,16 @@ export default function ConfirmationPage() {
       <div className="confirmation-scroll">
         {/* Delivery info */}
         <div className="delivery-info">
-          <h2 className="delivery-label">Got it!</h2>
+          <div className="delivery-header">
+            <h2 className="delivery-label">Got it!</h2>
+            <button
+              type="button"
+              className="return-button"
+              onClick={() => (window.location.href = '/')}
+            >
+              Return
+            </button>
+          </div>
           <h4 className="delivery-text">Looking for a driver...</h4>
           <p className="delivery-eta">Estimated Wait Time</p>
           <h2 className="delivery-time">3 hours</h2>
@@ -37,17 +42,23 @@ export default function ConfirmationPage() {
         <div className="navigation-tabs">
           <button type="button" className="nav-tab">
             <div className="nav-icon active">
-              <span className="icon"><CardHeading size={28} /></span>
+              <span className="icon">
+                <CardHeading size={28} />
+              </span>
             </div>
           </button>
           <button type="button" className="nav-tab">
             <div className="nav-icon">
-              <span className="icon"><CarFrontFill size={28} /></span>
+              <span className="icon">
+                <CarFrontFill size={28} />
+              </span>
             </div>
           </button>
           <button type="button" className="nav-tab">
             <div className="nav-icon">
-              <span className="icon"><House size={28} /></span>
+              <span className="icon">
+                <House size={28} />
+              </span>
             </div>
           </button>
         </div>
@@ -63,7 +74,9 @@ export default function ConfirmationPage() {
             <h2 className="status-title">Request Pending</h2>
             <span className="status-time">6:18 pm</span>
           </div>
-          <p className="status-message">We&apos;ll notify you once your driver has been assigned.</p>
+          <p className="status-message">
+            We&apos;ll notify you once your driver has been assigned.
+          </p>
         </div>
       </div>
     </div>
